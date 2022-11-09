f434b18be0ffd4ab1419d68791c934e69d013185
muhammadmoizrafiq <m4moiz2000@gmail.com>
2022-11-10 00:19:46 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,25 @@
+// var a =[ "a" , "b" , "c" , "d" , "e"];
+
+// a.push("f");
+
+// console.log(a);
+
+// a.pop();
+
+// console.log(a);
+
+// a.shift();
+
+// console.log(a);
+
+// a.unshift("f");
+
+// console.log(a);
+
+// a.splice("2,0 "1");
+
+// console.log(a);
+
+for ( var i= 0; i < 10; i++){
+    console.log("5 x " + ( i + 1 ) + " = " + 5 * (i + 1));
+}
